df134675fc5cce6683bc705475ed4a4f2e793bb1
hoysal08 <suraj_yahoo.com>
2023-04-15 20:48:44 +0530

    using hashrouter instead of browerrouter

--- src/App.js
+++ src/App.js
@@ -20,18 +20,18 @@ import Home from './pages/Home';
 import NewAccount from './pages/NewAccount';
 import NewAccountWithmnemonic from './pages/NewAccountWithmnemonic';
 import NewAddress from './pages/NewAddress';
-
+import { HashRouter } from 'react-router-dom';
 function App() {
 
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route exact path="/" element={<Home/>}/>
       <Route exact path="/newaccount" element={<NewAccount/>}/>
       <Route exact path="/newaccountwmnemonic" element={<NewAccountWithmnemonic/>}/>
       <Route exact path="/newAddress" element={<NewAddress/>}/>
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
